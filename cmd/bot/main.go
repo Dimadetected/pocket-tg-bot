432c821e6df796d4e8d961a2475442c296fdd69d
dmitry <dimadetected@yandex.ru>
2021-03-26 21:58:13 +0300

    бот + обработчики команд

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,35 +1,27 @@
 package main
 
 import (
+	"github.com/Dimadetected/pocket-tg-bot/pkg/telegram"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/zhashkevych/go-pocket-sdk"
 	"log"
 )
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI("1687025157:AAGbegSVrFWHwAOHp5eCSiqriC-4E18RkvQ")
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 
-	bot.Debug = true
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		bot.Send(msg)
+	pocketClient, err := pocket.NewClient("96523-8840334b96dcabb25386a179")
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	tgBot := telegram.NewBot(bot, pocketClient, "http://localhost")
+	err = tgBot.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Dimadetected/pocket-tg-bot
 go 1.15
 
 require (
-	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	github.com/zhashkevych/go-pocket-sdk v0.0.0-20201222091903-35b3b269a1e8
 )
